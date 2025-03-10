6c741b86550c94bc8f2e112642553c0b1e37fb2a
AlexOluwaseyi <akintolaoluwaseyi34@gmail.com>
2025-03-10 13:15:55 +0100

    Feat: Complete implementation for GET '/api/tests/:id'

--- src/app/api/tests/[id]/route.ts
+++ src/app/api/tests/[id]/route.ts
@@ -1,9 +1,53 @@
-export async function GET() {
-  return Response.json({ message: "Hello World" });
+import { NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
+import handlePrismaError from "@/lib/prismaErrorHandler";
+
+export async function GET(
+  request: Request,
+  { params }: { params: Promise<{ id: string }> }
+): Promise<Response> {
+  try {
+    // Fetch the ID from the request parameters
+    const { id } = await params;
+
+    // Check if no ID is provided
+    if (!id) {
+      return NextResponse.json(
+        { error: "No ID provided in request." },
+        { status: 400 }
+      );
+    }
+
+    // Find the record by ID using PrismaORM
+    const record = await prisma.diagnosticTest.findUnique({
+      where: { id: parseInt(id) },
+    });
+
+    // Check if no record is found
+    if (!record) {
+      return NextResponse.json(
+        { message: "Record not found." },
+        { status: 404 }
+      );
+    }
+
+    // Return the record and status 200
+    return NextResponse.json(
+      { message: "Record found", Record: record },
+      { status: 200 }
+    );
+  } catch (error) {
+    // Handle errors
+    const { status, message } = handlePrismaError(error);
+    return NextResponse.json({ error: message }, { status });
+  }
 }
+
 export async function PUT() {
   return Response.json({ message: "Hello World" });
 }
-export async function DELETE() {
-  return Response.json({ message: "Hello World" });
-}
+
+
+// export async function DELETE() {
+//   return Response.json({ message: "Hello World" });
+// }
